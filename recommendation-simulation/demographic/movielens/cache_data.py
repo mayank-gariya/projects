58bb07ff992ed9f48b822ab0a1c3129ee5158cd1
mayank-ml_dev <albert.creates85@gmail.com>
2026-06-28 21:26:20 +0530

    Update cache_data.py

--- recommendation-simulation/demographic/movielens/cache_data.py
+++ recommendation-simulation/demographic/movielens/cache_data.py
@@ -1,35 +1,42 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+
+# Get the project root
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+# Path to ml-100k folder
+DATA_DIR = BASE_DIR / "ml-100k"
+
 
 @st.cache_data
 def load_movies_data():
-    """Loads and formats the official MovieLens demographic pipeline."""
-    
+
     users = pd.read_csv(
-        "ml-100k/u.user",
+        DATA_DIR / "u.user",
         sep="|",
         names=["user_id", "age", "gender", "occupation", "zip_code"],
         encoding="latin-1"
     )
-    
+
     ratings = pd.read_csv(
-        'ml-100k/u.data',
-        sep='\t',
-        names=['user_id','movie_id','rating','timestamp']
+        DATA_DIR / "u.data",
+        sep="\t",
+        names=["user_id", "movie_id", "rating", "timestamp"]
     )
-    
+
     movie_cols = [
         "movie_id","title","release_date","video_release_date","IMDb_URL",
         "unknown","Action","Adventure","Animation","Children","Comedy",
         "Crime","Documentary","Drama","Fantasy","Film-Noir","Horror",
         "Musical","Mystery","Romance","Sci-Fi","Thriller","War","Western"
     ]
-    
+
     movies = pd.read_csv(
-        "ml-100k/u.item",
+        DATA_DIR / "u.item",
         sep="|",
         names=movie_cols,
         encoding="latin-1"
     )
-    
+
     return users, ratings, movies
