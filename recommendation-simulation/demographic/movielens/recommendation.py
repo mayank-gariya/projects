f51b42e651be737edc5fa55f9a4cdcd05ed718a0
mayank-ml_dev <albert.creates85@gmail.com>
2026-06-28 13:44:07 +0530

    Update recommendation.py

--- recommendation-simulation/demographic/movielens/recommendation.py
+++ recommendation-simulation/demographic/movielens/recommendation.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 import pandas as pd
 from recommendation_simulation.demographic.movielens.cache_data import load_movies_data
