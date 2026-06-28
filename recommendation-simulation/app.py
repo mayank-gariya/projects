5974e7b4bc391f61001b1d906c6084102542e685
mayank-ml_dev <albert.creates85@gmail.com>
2026-06-28 11:38:50 +0530

    Update app.py

--- recommendation-simulation/app.py
+++ recommendation-simulation/app.py
@@ -8,11 +8,11 @@ st.set_page_config(
 )
 
 pg = st.navigation([
-    st.Page('recommendation_simulation/home/home.py',title='Home'),
-    st.Page("recommendation_simulatio/demographic/main.py", title="Demographic"),
-    st.Page("recommendation_simulatio/contentrcs/content.py", title="Content Based"),
-    st.Page("recommendation_simulatio/collaborative/simulator.py", title="Collaborative Rec"),
-    st.Page('recommendation_simulatio/hybrid/hybrid.py',title='Hybrid Rec')
+    st.Page('recommendation-simulation/home/home.py',title='Home'),
+    st.Page("recommendation-simulatio/demographic/main.py", title="Demographic"),
+    st.Page("recommendation-simulatio/contentrcs/content.py", title="Content Based"),
+    st.Page("recommendation-simulatio/collaborative/simulator.py", title="Collaborative Rec"),
+    st.Page('recommendation-simulatio/hybrid/hybrid.py',title='Hybrid Rec')
 ], position="top")
 
 pg.run()
