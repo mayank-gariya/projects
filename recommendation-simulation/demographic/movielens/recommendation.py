488fdc6a858a9611e874d8efc01206857b30db1f
mayank-ml_dev <albert.creates85@gmail.com>
2026-06-28 13:42:26 +0530

    Update recommendation.py

--- recommendation-simulation/demographic/movielens/recommendation.py
+++ recommendation-simulation/demographic/movielens/recommendation.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from recommendation-simulation.demographic.movielens.cache_data import load_movies_data
+from recommendation_simulation.demographic.movielens.cache_data import load_movies_data
 
 users, ratings, movies = load_movies_data()
 
